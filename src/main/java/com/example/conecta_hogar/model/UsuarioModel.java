695b03a1c43b0cc8010faebee625cda70cd78db4
NicolasLuna2001 <nicolaslunaavila33@gmail.com>
2026-07-15 17:35:39 -0400

    creasion de odelos usuario, admin y roles

---
+++ src/main/java/com/example/conecta_hogar/model/UsuarioModel.java
@@ -0,0 +1,62 @@
+package com.example.conecta_hogar.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuario")
+@Inheritance(strategy = InheritanceType.JOINED)//se declara que esta tabla tendrá hijas (herencias)
+@Getter
+@Setter
+@NoArgsConstructor
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "El apellido es obligatorio")
+    @Size(min = 3, max = 100, message = "El apellido debe tener entre 3 y 100 caracteres")
+    @Column(nullable = false)
+    private String apellido;
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email(message = "Debe ingresar un correo válido")
+    @Column(nullable = false, unique = true)
+    private String correo;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 8, max = 100, message = "La contraseña debe tener al menos 8 caracteres")
+    private String contrasena;
+
+    @NotBlank(message = "El teléfono es obligatorio")
+    @Pattern(
+            regexp = "^[0-9]{9}$",
+            message = "El teléfono debe tener 9 dígitos"
+    )
+    private String telefono;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable =false)
+    private Rol rol;
+
+    @NotBlank(message = "El estado es obligatorio")
+    private String estado;
+
+    @Column(name = "fecha_registro")
+    private LocalDateTime fechaRegistro;
+}
