027ed17cb803aa0ffdbe3b73c96b8dd0374b9f1a
NicolasLuna2001 <nicolaslunaavila33@gmail.com>
2026-07-27 13:09:45 -0400

    se agrgan todos los archivos para el funcionamiento de especialidad

--- src/main/java/com/example/conecta_hogar/repository/EspecialidadRepository.java
+++ src/main/java/com/example/conecta_hogar/repository/EspecialidadRepository.java
@@ -4,6 +4,13 @@ import com.example.conecta_hogar.model.EspecialidadModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EspecialidadRepository extends JpaRepository<EspecialidadModel, Long> {
+
+    boolean existsByNombre(String nombre);
+
+    Optional<EspecialidadModel> findByNombre(String nombre);
+
 }
