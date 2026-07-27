5f81349fe912ec43947157ce8ad1d8bc93bb2074
NicolasLuna2001 <nicolaslunaavila33@gmail.com>
2026-07-27 14:49:00 -0400

    se agrgan todos los archivos para el funcionamiento de la entidad intermedia maestroespecialida

--- src/main/java/com/example/conecta_hogar/repository/MaestroEspecialidadRepository.java
+++ src/main/java/com/example/conecta_hogar/repository/MaestroEspecialidadRepository.java
@@ -3,11 +3,25 @@ package com.example.conecta_hogar.repository;
 import com.example.conecta_hogar.model.MaestroEspecialidadModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
-public interface MaestroEspecialidadRepository extends JpaRepository<MaestroEspecialidadModel, Long> {
+public interface MaestroEspecialidadRepository
+        extends JpaRepository<MaestroEspecialidadModel, Long> {
 
-    // Método para obtener todas las especialidades asociadas a un maestro por su ID
-    List<MaestroEspecialidadModel> findByMaestroIdUsuario(Long idUsuario);
+    List<MaestroEspecialidadModel> findByMaestroIdUsuario(Long idMaestro);
+
+    List<MaestroEspecialidadModel>
+    findByEspecialidadIdEspecialidad(Long idEspecialidad);
+
+    boolean existsByMaestroIdUsuarioAndEspecialidadIdEspecialidad(
+            Long idMaestro,
+            Long idEspecialidad
+    );
+
+    void deleteByMaestroIdUsuarioAndEspecialidadIdEspecialidad(
+            Long idMaestro,
+            Long idEspecialidad
+    );
 }
